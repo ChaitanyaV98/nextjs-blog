fd3c9a40d67e040dc0a3294f44851e9081a8aee8
ChaitanyaV98 <chaitanya@betalectic.com>
2025-04-27 17:43:47 +0530

    minor change

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,22 +1,13 @@
 import BlogsOverview from "@components/blog-overview";
 import { BASE_URL } from "@/config/env";
 
-async function fetchListOfBlogs() {
-  try {
-    const res = await fetch(`${BASE_URL}/api/get-blogs`, {
-      method: "GET",
-      cache: "no-store",
-    });
-    const data = await res.json();
-    return data.data;
-  } catch (error) {
-    console.error("Error fetching blogs:", error);
-    return [];
-  }
-}
-
 export default async function Blogs() {
-  const blogsList = await fetchListOfBlogs();
+  const data = await fetch(`${BASE_URL}/api/get-blogs`, {
+    method: "GET",
+    cache: "no-store",
+  });
+  const jsonData = await data.json();
+  const blogsList = jsonData.data;
 
   return <BlogsOverview blogsList={blogsList} />;
 }
--- src/database/index.js
+++ src/database/index.js
@@ -1,41 +1,13 @@
-// import mongoose from "mongoose";
-
-// const connectToDB = async () => {
-//   try {
-//     const connectionUrl = process.env.MONGO_URL;
-//     await mongoose.connect(connectionUrl);
-//     console.log("Successfully connected to db");
-//   } catch (error) {
-//     console.log("Connection to db failed!!", error);
-//     return error;
-//   }
-// };
-
-// export default connectToDB;
-
 import mongoose from "mongoose";
 
 const connectToDB = async () => {
   try {
     const connectionUrl = process.env.MONGO_URL;
-
-    if (!connectionUrl) {
-      throw new Error("MONGO_URL environment variable is not defined!");
-    }
-
-    if (mongoose.connections[0].readyState) {
-      console.log("Already connected to MongoDB.");
-      return;
-    }
-
-    await mongoose.connect(connectionUrl, {
-      dbName: "your-db-name-here", // optional but good practice
-    });
-
-    console.log("Successfully connected to db 🚀");
+    await mongoose.connect(connectionUrl);
+    console.log("Successfully connected to db");
   } catch (error) {
-    console.error("Connection to db failed!!", error);
-    throw error; // important! propagate error upward
+    console.log("Connection to db failed!!", error);
+    return error;
   }
 };
 
